25246aa90a6247b34636d5d77e74c163e0cb8010
lilouNait <38019413+lilouNait@users.noreply.github.com>
2019-05-20 15:21:55 +0200

    Adding autowired in the garageApiController

--- src/main/java/io/swagger/api/GaragesApiController.java
+++ src/main/java/io/swagger/api/GaragesApiController.java
@@ -10,6 +10,7 @@ import io.swagger.model.InlineResponse2001;
 import io.swagger.repository.GarageDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.reactive.error.DefaultErrorWebExceptionHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,6 +30,7 @@ import java.io.IOException;
 @Controller
 public class GaragesApiController implements GaragesApi {
 
+    @Autowired
     private GarageMapper garageMapper;
     private static final Logger log = LoggerFactory.getLogger(GaragesApiController.class);
 
